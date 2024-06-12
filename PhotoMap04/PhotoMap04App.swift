a9fd805ab7d6d8897fa48eda015ca0d54a90d75f
23cm0104 <23cm0104@jec.ac.jp>
2024-06-12 20:57:25 +0900

    Initial Commit

---
+++ PhotoMap04/PhotoMap04App.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoMap04App.swift
+//  PhotoMap04
+//
+//  Created by cmStudent on 2024/06/12.
+//
+
+import SwiftUI
+
+@main
+struct PhotoMap04App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PhotoMap04Tests/PhotoMap04Tests.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoMap04Tests.swift
+//  PhotoMap04Tests
+//
+//  Created by cmStudent on 2024/06/12.
+//
+
+import Testing
+@testable import PhotoMap04
+
+struct PhotoMap04Tests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
